bcab05efaab3d119107b666bd11adcc027359cbb
Abhi Khatri <abhikhatri391@gmail.com>
2017-01-20 00:43:03 +0530

    Redesign Init

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+//Gulp build task dependencies
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css'),
+    prefix = require('gulp-autoprefixer');
+
+// Function to print errors on console without terminating gulp
+function errorLog(error){
+  console.error.bind(error);
+  this.emit('end');
+}
+
+//Compress and concat JS File
+gulp.task('script', function(){
+    return gulp.src(['./js/vendor/*.js', './js/*.js'])
+    .pipe(concat('script.js'))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .on('error', errorLog)
+    .pipe(gulp.dest('./dist/js'));
+});
+
+//Compile, minify and autoprefixes all sass files
+gulp.task('sass', function(){
+    gulp.src('./sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(prefix('last 5 versions'))
+    .pipe(cleanCSS({compatibility: 'ie9'}))
+    .pipe(rename({ 
+      basename: 'style',
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./dist/css'));
+
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./js/*.js', ['script']);
+});
+
+
+gulp.task('default', ['script', 'style']);
+
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    $("body").hide(0).delay(400).fadeIn(2000)
+  
 });
